9be56dc3aa5d48e8faf5ad65afd98dfd51a9b304
Sultan466 <96310947+Sultan466@users.noreply.github.com>
2023-10-31 21:27:32 +0500

    intial code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>Hello this will be the website</code> change the file App.js
         </p>
         <a
           className="App-link"
